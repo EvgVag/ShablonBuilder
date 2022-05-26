8287c68f9130d5ed8ebd368b471c6035ce522b98
evgeny <evgeniyvaganov@yandex.ru>
2022-05-26 21:12:18 +0500

    commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,74 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        if (age == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasAddress() {
+        if (address == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (age != 0) {
+            age++;
+        }
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAddress(address).setAge(15);
+    }
+
+    @Override
+    public String toString() {
+        return name ;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
